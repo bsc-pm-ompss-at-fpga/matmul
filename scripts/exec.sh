ce6078a6688130062c9252695217b14d55fc62d9
Miquel Vidal i Pinyol <miquel.vidal@bsc.es>
2024-06-03 16:47:35 +0200

    Do not override NANOS6_CONFIG_OVERRIDE variable

--- scripts/exec.sh
+++ scripts/exec.sh
@@ -11,12 +11,14 @@ declare -A RUNTIME_MODE_EXEC_MODE
 RUNTIME_MODE_EXEC_MODE['d']="debug"
 RUNTIME_MODE_EXEC_MODE['p']="perf"
 
+# Do not override NANOS6_CONFIG_OVERRIDE, as it might contain node-specific default values
+
 for EXEC_MODE in d p; do
   for CREATE_FROM in 0 1; do
     for MATRIX_SIZE in ${MATRIX_SIZES[@]}; do
       echo "=== Check mode: ${EXEC_MODE}, from: ${CREATE_FROM}, msize: ${MATRIX_SIZE} ==="
       CHECK=$([ "$MATRIX_SIZE" == "2048" ] && echo 1 || echo 0)
-      NANOS6_CONFIG_OVERRIDE=${NANOS6_CONFIG_EXEC_MODE[$EXEC_MODE]} \
+      NANOS6_CONFIG_OVERRIDE="$NANOS6_CONFIG_OVERRIDE,${NANOS6_CONFIG_EXEC_MODE[$EXEC_MODE]}" \
       RUNTIME_MODE=${RUNTIME_MODE_EXEC_MODE[$EXEC_MODE]} \
         ./build/matmul-${EXEC_MODE} ${MATRIX_SIZE} ${CHECK} ${CREATE_FROM}
       cat test_result.json >>$RES_FILE
